ffd6623f79c67d951a786341e8f305ba3eaa0177
Alireza-S.N <alireza6677@gmail.com>
2016-12-24 22:59:13 +0330

    Error checkings added and lists reversed

--- SubtitleGet.py
+++ SubtitleGet.py
@@ -22,6 +22,30 @@ def usage():
     print("Usage : ./SubtitleGet.py 'Movie/Serial Name'\n")
     exit(1)
 
+def getPageContent(url):
+    page = ""
+    try:
+        page = requests.get(url)
+        return page.content
+    except:
+        print("Connection error !\nCheck your internet connection and try again.")
+        exit(1)
+
+def downloadFile(url, path):
+    resp = ""
+    try:
+        resp = urllib.request.urlopen(url).read()
+    except:
+        print("Error downloading the file.\nCheck your internet connection and try agian")
+        exit(1)
+    try:
+        output =  open(path , "wb")
+        output.write(resp)
+        output.close()
+    except:
+        print("Error saving the file.\nCheck your disk space or this path:\n"+path)
+        exit(1)
+
 def main():
     movie = ""
     if len(sys.argv) > 1:
@@ -32,7 +56,7 @@ def main():
     else:
         usage()
 
-    page = requests.get("http://sub.salamdl.ir/subtitles/title?q=" + movie).content
+    page = getPageContent("http://sub.salamdl.ir/subtitles/title?q=" + movie)
     bs = bs4.BeautifulSoup(page , "html.parser")
     
     items = bs.find_all("div" , {"class" : "title"})
@@ -47,11 +71,11 @@ def main():
         print("Error : Movie not found !")
         exit(1)
 
-    i = 0
-    while i < len(names):
+    i = len(names) - 1
+    while i >= 0:
         #item = "[%d] " % i+1
         print(" ["+str(i+1)+"] " + names[i])
-        i += 1
+        i -= 1
     inp = input("\nChoose one of the movies above (1 - %d): " % len(names)).strip()
     
     try:
@@ -67,7 +91,7 @@ def main():
         print("\n")
 
     link = "http://sub.salamdl.ir" + links[inp - 1] + "/farsi_persian"
-    bs2 = bs4.BeautifulSoup(requests.get(link).content , "html.parser")
+    bs2 = bs4.BeautifulSoup(getPageContent(link)  , "html.parser")
     h = bs2.find_all("span" , {"class" : "positive-icon"})
     for i in h:
         i.decompose()
@@ -87,10 +111,10 @@ def main():
         print("No persian subtitles found :(")
         exit()
 
-    i = 0
-    while i < len(subtitles):
+    i = len(subtitles) - 1 
+    while i >= 0:
         print(" ["+str(i+1)+"] " + subtitles[i])
-        i += 1
+        i -= 1
 
     inp2 = input("\nChoose one of subtitles above (1 - %d): " % len(subtitles)).strip()
     
@@ -109,17 +133,12 @@ def main():
     link2 = "http://sub.salamdl.ir"  + sublinks[inp2 - 1]
     name2 = subtitles[inp2 - 1]
 
-    bs3 = bs4.BeautifulSoup(requests.get(link2).content , "html.parser")
+    bs3 = bs4.BeautifulSoup(getPageContent(link2) , "html.parser")
     downloadLink = "http://sub.salamdl.ir" +  bs3.find("a" , {"id":"downloadButton"})['href']
-    resp = urllib.request.urlopen(downloadLink).read()
-    
     path = os.path.expanduser("~")+"/Downloads/" + name2 + ".zip" 
-    output =  open(path , "wb")
-    output.write(resp)
-    output.close()
-
+    
+    downloadFile(downloadLink , path)
     print("\n\nAll Done.\n saved to :" + path)
-
 
 
 if __name__ == "__main__":
